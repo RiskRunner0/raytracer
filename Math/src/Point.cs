849ac0dc79e9b1b8b7803984961bc8d2dc6178e4
Matt Smith <matthew.j.smith2@outlook.com>
2021-01-01 10:15:30 -0800

    Adding gitignore, some very basic folder and sln structure

---
+++ Math/src/Point.cs
@@ -0,0 +1,11 @@
+namespace src
+{
+    using System;
+
+    public class Point
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+    }
+}
---
+++ Math/src/Vector.cs
@@ -0,0 +1,9 @@
+﻿namespace src
+{
+    using System;
+
+    public class Vector : Point
+    {
+        public float w { get; set; }
+    }
+}
