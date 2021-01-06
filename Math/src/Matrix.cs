3742771235236c7dd7283ded957454966eb8640f
Matt Smith <matthew.j.smith2@outlook.com>
2021-01-05 18:55:36 -0800

    Adding base for matricies

---
+++ Math/src/Matrix.cs
@@ -0,0 +1,43 @@
+﻿namespace Raytracer.Math
+{
+    public class Matrix
+    {
+        private float[,] internalMatrix;
+
+        private readonly int rows;
+        private readonly int columns;
+
+        public Matrix(int rows, int columns)
+        {
+            this.internalMatrix = new float[rows, columns];
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public Matrix(int squareSize)
+            :this(squareSize, squareSize)
+        { }
+
+        public void Set(int row, int column, float value) => this.internalMatrix[row, column] = value;
+
+        public float Get(int row, int column) => this.internalMatrix[row, column];
+
+        public static bool AreEqual(Matrix a, Matrix b)
+        {
+            if (a.rows != b.rows || a.columns != b.columns) return false;
+
+            for(int row = 0; row < a.rows; ++row)
+            {
+                for(int column = 0; column < a.columns; ++column)
+                {
+                    if(a.Get(row, column) != b.Get(row, column))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Math/tst/MatrixTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Raytracer.Math.Tests
+{
+    [TestClass]
+    public class MatrixTests
+    {
+        [TestMethod]
+        public void MatriciesAreTheSame()
+        {
+            var a = new Matrix(2);
+            var b = new Matrix(2);
+
+            a.Set(0, 0, 1);
+            a.Set(0, 1, 2);
+            a.Set(1, 0, 3);
+            a.Set(1, 1, 4);
+
+            b.Set(0, 0, 1);
+            b.Set(0, 1, 2);
+            b.Set(1, 0, 3);
+            b.Set(1, 1, 4);
+
+            Assert.IsTrue(Matrix.AreEqual(a, b));
+        }
+
+        [TestMethod]
+        public void MatriciesThatAreNotSimilarDimensionsAreNotEqual()
+        {
+            var a = new Matrix(2);
+            var b = new Matrix(3);
+
+            Assert.IsFalse(Matrix.AreEqual(a, b));
+        }
+
+        [TestMethod]
+        public void MatriciesAreNotEqual()
+        {
+            var a = new Matrix(2);
+            var b = new Matrix(2);
+
+            a.Set(0, 0, 1);
+            a.Set(0, 1, 2);
+            a.Set(1, 0, 3);
+            a.Set(1, 1, 4);
+
+            b.Set(0, 0, 2);
+            b.Set(0, 1, 3);
+            b.Set(1, 0, 4);
+            b.Set(1, 1, 5);
+
+            Assert.IsFalse(Matrix.AreEqual(a, b));
+        }
+    }
+}
