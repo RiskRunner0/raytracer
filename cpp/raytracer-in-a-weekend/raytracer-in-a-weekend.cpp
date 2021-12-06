4d90f24a0b4275351df48df55fc1aec53d20c36c
Matt Smith <matthew.j.smith2@outlook.com>
2021-12-05 21:38:57 -0800

    Adding CPP project, finished 6.1 of Raytracer in a Weekend

---
+++ cpp/raytracer-in-a-weekend/raytracer-in-a-weekend.cpp
@@ -0,0 +1,79 @@
+// raytracer-in-a-weekend.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+#include <iostream>
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
+color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+    // Image
+    const auto aspectRatio = 16.0 / 9.0;
+
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspectRatio);
+
+    // Camera
+    auto viewportHeight = 2.0;
+    auto viewportWidth = aspectRatio * viewportHeight;
+    auto focalLength = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewportWidth, 0, 0);
+    auto vertical = vec3(0, viewportHeight, 0);
+    auto lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focalLength);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+            color pixelColor = ray_color(r);
+            writeColor(std::cout, pixelColor);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
