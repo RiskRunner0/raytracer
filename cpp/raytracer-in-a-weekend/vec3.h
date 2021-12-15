39e058fa2eb4eed4a2762253696a6dc0512757eb
Matt Smith <matthew.j.smith2@outlook.com>
2021-12-14 17:20:08 -0800

    Finished Chapter 9

--- cpp/raytracer-in-a-weekend/vec3.h
+++ cpp/raytracer-in-a-weekend/vec3.h
@@ -64,6 +64,11 @@ public:
 	{
 		return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
 	}
+
+	bool near_zero() const {
+		const auto s = 1e-8;
+		return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+	}
 };
 
 // Type aliases for vec3
@@ -150,6 +155,10 @@ vec3 random_in_hemisphere(const vec3 &normal)
 	{
 		return -in_unit_sphere;
 	}
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+	return v - 2 * dot(v, n) * n;
 }
 
 #endif
